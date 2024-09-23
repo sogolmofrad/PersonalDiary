b718df620a43a51b639e94a89025adb0f05d4cb4
sogol mofrad <sogolmofrad@gmail.com>
2024-09-23 18:54:05 +0200

    add border images to main tag

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,18 @@ function Home() {
   return (
     <div>
       <Header />
-      <main></main>
+      <main className="main w-[90%] flex mx-auto relative h-[85vh]">
+        <img
+          src="images/border-image-vertical.png"
+          alt="border-right"
+          className="absolute left-0 h-[95%]"
+        />
+        <img
+          src="images/border-image-vertical.png"
+          alt="border-right"
+          className="absolute right-0 h-[95%]"
+        />
+      </main>
     </div>
   );
 }
