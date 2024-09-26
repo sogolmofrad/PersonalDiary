04bb13c784dba7ce6255364fa8c1bbe7b3a52bff
Sylwia Kruk <sylviaamj@gmail.com>
2024-09-26 14:54:51 +0200

    Added single card

--- src/components/SingleDiary.jsx
+++ src/components/SingleDiary.jsx
@@ -1,5 +1,11 @@
-function SingleDiary() {
-  return <div className="diary-card"></div>;
+function SingleDiary({title, mood, weather, picture, description}) {
+  return <div className="diary-card min-h-[500px] border-solid flex flex-col items-center">
+<h2 className="text-4xl">{title}</h2>
+  <p>{mood}</p>
+  <p>{weather}</p>
+  <img src={picture} alt={title}/>
+  <p>{description}</p>
+    </div>;
 }
 
 export default SingleDiary;
